21b58855025632462a09ccf38895db08df368fc8
Be1o4ka777 <118620337+Be1o4ka777@users.noreply.github.com>
2023-10-15 13:06:10 +0300

    Update Python.py

--- Python.py
+++ Python.py
@@ -1,6 +1,9 @@
 import discord
 from discord.ext import commands
 
+
+
+
 intents = discord.Intents.default()
 intents.message_content = True
 
@@ -10,21 +13,21 @@ bot = commands.Bot(command_prefix='$', intents=intents)
 async def on_ready():
     print(f'We have logged in as {bot.user}')
 
-@bot.command()
-async def hello(ctx):
-    await ctx.send(f'Привет! Я бот {bot.user}!')
+
+
+x1 = ['Пластиковые_бутылки','Пластиковые_упаковки','Одноразовая_пластиковая посуда','Полителеновые_пакеты']
+x2 = ['Бумага',"Бумажные_упаковки","Одноразовая_бумажная_посуда"]
+x3 = ["Стеклянные_бутылки "]
 
 @bot.command()
-async def heh(ctx, count_heh = 5):
-    await ctx.send("he" * count_heh)
+async def eco(ctx, my1):
+  if my1 in x1:
+    await ctx.send("Контейнер для пластика")
+  elif my1 in x2:
+    await ctx.send("Контейнер для бумаги")
+  elif my1 in x3:
+    await ctx.send("Контейнер для стекла")
+  else:
+    await ctx.send("Попробуйте еще раз")
 
-@bot.command()
-async def repeat(ctx, times: int, content='repeating...'):
-    """Repeats a message multiple times."""
-    for i in range(times):
-        await ctx.send(content)
-
-@cool.command(name='bot')
-async def _bot(ctx):
-    """Is the bot cool?"""
-    await ctx.send('Yes, the bot is cool.'
+bot.run("")
